8850ecb12716cada624cba28fe60ee949fe52e28
julia1234567891011 <juliagbressan@hotmail.com>
2018-08-14 08:48:42 -0300

    criado classe produto e encapsulado

---
+++ venda/src/venda/sistema/Produto.java
@@ -0,0 +1,45 @@
+package venda.sistema;
+
+public class Produto {
+	
+		private String nome;
+		private double precoDeCompra;
+		private int quantidadeDeEstoque;
+		public Produto(String nome, double precoDeCompra, int quantidadeDeEstoque){
+			this.nome = nome;
+			this.precoDeCompra = precoDeCompra;
+			this.quantidadeDeEstoque = quantidadeDeEstoque;
+			}
+		public Produto(){
+		}
+		public void setNome(String nome){
+			this.nome = nome;
+			}
+		public String getNome(){
+			return this.nome;
+			}
+		public void setPrecoDeCompra(double valor){
+			this.precoDeCompra = valor;
+			}
+		public double getPrecoDeCompra(){
+			return this.precoDeCompra;
+			}
+		public void setQuantidadeDeEstoque(int valor){
+			this.quantidadeDeEstoque = valor;
+			}
+		public int getQuantidadeDeEstoque(){
+			return this.quantidadeDeEstoque;
+			}
+		public double definePrecoDeVenda(double porcentagem){
+			return precoDeCompra += precoDeCompra*(porcentagem/100.0);
+			}
+		public void vende(int quantidade){
+			if(this.quantidadeDeEstoque <= quantidade){
+				this.quantidadeDeEstoque-=quantidade;
+			}
+			else{
+				System.out.println("Quantidade requerida indisponivel no estoque");
+			}
+			}
+	}
+
